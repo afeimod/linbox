442b96c52c8485411e019218165de276dcb0dece
afei <165532911+afeimod@users.noreply.github.com>
2025-11-06 14:32:15 +0800

    Add files via upload

--- path/setup_chroot.sh
+++ path/setup_chroot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+echo "开始设置 chroot 环境..."
+
 # 设置软件源
 cat > /etc/apt/sources.list << 'SOURCES'
 deb http://archive.ubuntu.com/ubuntu/ bionic main universe multiverse
@@ -14,9 +16,17 @@ apt install -y software-properties-common
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 apt update
 
-# 安装构建工具
+# 安装 GCC 11 和构建工具
+apt install -y gcc-11 g++-11 gcc-11-multilib g++-11-multilib
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 100
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 100
+update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-11 100
+update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-11 100
+
+# 安装基本构建工具
 apt install -y build-essential autoconf automake flex bison libtool \
-  pkg-config gettext nasm yasm cmake meson ninja-build
+  pkg-config gettext nasm yasm cmake meson ninja-build \
+  libc6-dev libc6-dev-i386
 
 # 安装开发库
 apt install -y libx11-dev libfreetype6-dev libfontconfig1-dev \
@@ -25,7 +35,8 @@ apt install -y libx11-dev libfreetype6-dev libfontconfig1-dev \
   libpng-dev libtiff5-dev libmpg123-dev \
   libcups2-dev libosmesa6-dev libpcap-dev libusb-1.0-0-dev \
   libsane-dev libv4l-dev libgphoto2-dev liblcms2-dev \
-  libpcsclite-dev libacl1-dev libxml2-dev libxslt1-dev
+  libpcsclite-dev libacl1-dev libxml2-dev libxslt1-dev \
+  libvulkan-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
 
 # 安装交叉编译器
 apt install -y gcc-mingw-w64 g++-mingw-w64
@@ -34,6 +45,22 @@ update-alternatives --set x86_64-w64-mingw32-g++ /usr/bin/x86_64-w64-mingw32-g++
 update-alternatives --set i686-w64-mingw32-gcc /usr/bin/i686-w64-mingw32-gcc-posix
 update-alternatives --set i686-w64-mingw32-g++ /usr/bin/i686-w64-mingw32-g++-posix
 
+# 安装 ccache
+apt install -y ccache
+
+# 创建必要的符号链接
+ln -sf /usr/bin/gcc-11 /usr/bin/gcc
+ln -sf /usr/bin/g++-11 /usr/bin/g++
+
+# 验证编译器
+echo "验证编译器安装..."
+gcc --version
+g++ --version
+x86_64-w64-mingw32-gcc --version
+i686-w64-mingw32-gcc --version
+
 # 清理
 apt clean
 rm -rf /var/lib/apt/lists/*
+
+echo "chroot 环境设置完成"
