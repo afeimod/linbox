4977e9f21e5c2f03bd49547a524c5f08af20579f
afei <165532911+afeimod@users.noreply.github.com>
2025-11-06 18:08:18 +0800

    Add files via upload

--- path/setup_chroot.sh
+++ path/setup_chroot.sh
@@ -14,59 +14,88 @@ SOURCES
 echo "启用多架构支持..."
 dpkg --add-architecture i386
 
-# 更新和安装基本工具
+# 更新包列表
 apt update
-apt install -y software-properties-common
+
+# 安装基本工具和依赖
+apt install -y software-properties-common apt-utils
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 apt update
 
+# 安装基础构建工具
+apt install -y build-essential autoconf automake flex bison libtool \
+  pkg-config gettext nasm yasm cmake meson ninja-build
+
+# 安装核心开发库（先安装64位）
+echo "安装核心开发库（64位）..."
+apt install -y \
+  libc6-dev \
+  libx11-dev libfreetype6-dev libfontconfig1-dev \
+  libasound2-dev libpulse-dev libdbus-1-dev libudev-dev \
+  libsdl2-dev libgnutls28-dev libldap2-dev \
+  libjpeg-dev libpng-dev libtiff5-dev \
+  libcups2-dev libosmesa6-dev libpcap-dev libusb-1.0-0-dev \
+  libsane-dev libv4l-dev libgphoto2-dev liblcms2-dev \
+  libpcsclite-dev libacl1-dev libxml2-dev libxslt1-dev \
+  libvulkan-dev
+
+# 安装多媒体相关库（64位）
+echo "安装多媒体库（64位）..."
+apt install -y \
+  libgstreamer1.0-dev \
+  libgstreamer-plugins-base1.0-dev \
+  libgstreamer-plugins-good1.0-dev \
+  libgstreamer-plugins-bad1.0-dev \
+  gstreamer1.0-plugins-base \
+  gstreamer1.0-plugins-good \
+  gstreamer1.0-plugins-bad \
+  gstreamer1.0-libav \
+  libmpg123-dev \
+  libopenal-dev
+
+# 安装 ORC 工具（解决依赖问题）
+apt install -y orc liborc-0.4-0 liborc-0.4-dev
+
 # 安装 GCC 11 和构建工具
+echo "安装 GCC 11..."
 apt install -y gcc-11 g++-11 gcc-11-multilib g++-11-multilib
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 100
 update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 100
 update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-11 100
 update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-11 100
 
-# 安装基本构建工具
-apt install -y build-essential autoconf automake flex bison libtool \
-  pkg-config gettext nasm yasm cmake meson ninja-build \
-  libc6-dev libc6-dev-i386
-
-# 安装开发库（64位和32位）
-echo "安装开发库..."
-apt install -y libx11-dev libfreetype6-dev libfontconfig1-dev \
-  libasound2-dev libpulse-dev libdbus-1-dev libudev-dev \
-  libsdl2-dev libgnutls28-dev libldap2-dev libjpeg-dev \
-  libpng-dev libtiff5-dev libmpg123-dev \
-  libcups2-dev libosmesa6-dev libpcap-dev libusb-1.0-0-dev \
-  libsane-dev libv4l-dev libgphoto2-dev liblcms2-dev \
-  libpcsclite-dev libacl1-dev libxml2-dev libxslt1-dev \
-  libvulkan-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
-
-# 安装32位开发库
+# 现在安装32位开发库
 echo "安装32位开发库..."
-apt install -y libx11-dev:i386 libfreetype6-dev:i386 libfontconfig1-dev:i386 \
-  libasound2-dev:i386 libpulse-dev:i386 libdbus-1-dev:i386 libudev-dev:i386 \
-  libsdl2-dev:i386 libgnutls28-dev:i386 libldap2-dev:i386 libjpeg-dev:i386 \
-  libpng-dev:i386 libtiff5-dev:i386 libmpg123-dev:i386 \
-  libcups2-dev:i386 libosmesa6-dev:i386 libpcap-dev:i386 libusb-1.0-0-dev:i386 \
-  libsane-dev:i386 libv4l-dev:i386 libgphoto2-dev:i386 liblcms2-dev:i386 \
-  libpcsclite-dev:i386 libacl1-dev:i386 libxml2-dev:i386 libxslt1-dev:i386 \
-  libvulkan-dev:i386 libgstreamer1.0-dev:i386 libgstreamer-plugins-base1.0-dev:i386
+apt install -y libc6-dev-i386
+
+# 安装32位基础库
+apt install -y \
+  libx11-dev:i386 libfreetype6-dev:i386 libfontconfig1-dev:i386 \
+  libasound2-dev:i386 libpulse-dev:i386 libdbus-1-dev:i386 \
+  libudev-dev:i386 libsdl2-dev:i386 libgnutls28-dev:i386 \
+  libjpeg-dev:i386 libpng-dev:i386 libtiff5-dev:i386 \
+  libcups2-dev:i386 libosmesa6-dev:i386 \
+  libxml2-dev:i386 libxslt1-dev:i386
+
+# 安装32位多媒体库
+echo "安装32位多媒体库..."
+apt install -y \
+  libgstreamer1.0-dev:i386 \
+  libgstreamer-plugins-base1.0-dev:i386 \
+  libgstreamer-plugins-good1.0-dev:i386 \
+  libmpg123-dev:i386 \
+  libopenal-dev:i386
+
+# 安装32位 ORC 工具
+apt install -y liborc-0.4-0:i386 liborc-0.4-dev:i386
 
 # 安装交叉编译器
+echo "安装交叉编译器..."
 apt install -y gcc-mingw-w64 g++-mingw-w64
 update-alternatives --set x86_64-w64-mingw32-gcc /usr/bin/x86_64-w64-mingw32-gcc-posix
 update-alternatives --set x86_64-w64-mingw32-g++ /usr/bin/x86_64-w64-mingw32-g++-posix
 update-alternatives --set i686-w64-mingw32-gcc /usr/bin/i686-w64-mingw32-gcc-posix
 update-alternatives --set i686-w64-mingw32-g++ /usr/bin/i686-w64-mingw32-g++-posix
-
-# 安装多媒体相关库
-echo "安装多媒体库..."
-apt install -y \
-  libgstreamer-plugins-good1.0-dev libgstreamer-plugins-bad1.0-dev \
-  gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-libav \
-  libgstreamer-plugins-good1.0-dev:i386 libgstreamer-plugins-bad1.0-dev:i386
 
 # 安装 ccache
 apt install -y ccache
@@ -75,16 +104,16 @@ apt install -y ccache
 ln -sf /usr/bin/gcc-11 /usr/bin/gcc
 ln -sf /usr/bin/g++-11 /usr/bin/g++
 
-# 验证编译器
+# 验证安装
 echo "验证编译器安装..."
 gcc --version
 g++ --version
 x86_64-w64-mingw32-gcc --version
 i686-w64-mingw32-gcc --version
 
-# 验证32位库
 echo "验证32位库..."
-find /usr/lib/i386-linux-gnu -name "*.so" | head -5
+find /usr/lib/i386-linux-gnu -name "libgstreamer*" 2>/dev/null | head -5
+find /usr/lib/i386-linux-gnu -name "liborc*" 2>/dev/null | head -5
 
 # 清理
 apt clean
